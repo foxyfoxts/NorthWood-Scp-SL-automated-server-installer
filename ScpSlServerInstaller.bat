a1dc6b27c3b3997b7ddd312d77f318f01a2fb492
Foxy Foxt <omermertogun@gmail.com>
2025-04-11 13:52:24 +0200

    V.2

--- ScpSlServerInstaller.bat
+++ ScpSlServerInstaller.bat
@@ -1,35 +1,47 @@
 @echo off
 setlocal enabledelayedexpansion
 
+:: Configuration
 set "steamcmd_url=https://steamcdn-a.akamaihd.net/client/installer/steamcmd.zip"
 set "steamcmd_dir=%~dp0steamcmd"
 set "server_dir=%~dp0SCP-SL-Server"
-set "total_steps=3"
+set "total_steps=5"
 set "current_step=0"
 set "progress=0"
 set "spinner=0"
+set "beta_params="
+set "version_file=%server_dir%\version.info"
 
-title Northwood SCP:SL Server Creator
+title Northwood SCP:SL Server Manager
 
+:: UI Settings
 set "bar_filled=#"
 set "bar_empty=-"
 set "spinner_chars=|\-/"
 
+:: Create directories
 if not exist "%steamcmd_dir%" mkdir "%steamcmd_dir%"
 if not exist "%server_dir%" mkdir "%server_dir%"
 cd /d "%~dp0"
 
+:: Check existing installation
+if exist "%server_dir%\LocalAdmin.exe" (
+    if exist "%version_file%" (
+        set /p "current_version=" < "%version_file%"
+    ) else (
+        set "current_version=Public Release (Current Game Version)"
+    )
+    goto :management_menu
+)
+
+:: Main installation flow
 call :initialize_display
 call :install_steamcmd
+call :select_version
 call :download_server
 call :complete_display
-
-:show_wiki_prompt
-echo.
-choice /c yn /n /m "Open official server guide? (Y/N): "
-if errorlevel 2 exit /b
-start "" "https://nwbooktest.itsgamertime.xyz/books/servers"
-exit /b
+call :post_install_prompt
+goto :management_menu
 
 :install_steamcmd
 call :update_progress "Installing SteamCMD..."
@@ -37,35 +49,103 @@ if exist "%steamcmd_dir%\steamcmd.exe" (
     set /a "current_step+=1"
     exit /b
 )
+
 if not exist "%steamcmd_dir%\steamcmd.zip" (
     powershell -Command "$ProgressPreference = 'SilentlyContinue'; Invoke-WebRequest -Uri '%steamcmd_url%' -OutFile '%steamcmd_dir%\steamcmd.zip'"
 )
+
 if not exist "%steamcmd_dir%\steamcmd.exe" (
     powershell -Command "Expand-Archive -Path '%steamcmd_dir%\steamcmd.zip' -DestinationPath '%steamcmd_dir%' -Force"
 )
 set /a "current_step+=1"
 exit /b
 
-:download_server
-call :update_progress "Downloading SCP:SL Server..."
-if exist "%server_dir%\LocalAdmin.exe" (
-    set /a "current_step+=1"
-    exit /b
+:select_version
+echo.
+echo ===== Version Selection =====
+echo 1. Public Release (Current Game Version)
+echo 2. Public Testing
+echo 3. Experimental
+echo 4. Custom Beta Code
+choice /c 1234 /n /m "Select version [1-4]: "
+set "beta_code="
+if errorlevel 4 (
+    set /p "beta_code=Enter beta code: "
+    set "beta_params=-beta %beta_code%"
+    set "current_version=%beta_code%"
+) else if errorlevel 3 (
+    set "beta_params=-beta experimental"
+    set "current_version=Experimental"
+) else if errorlevel 2 (
+    set "beta_params=-beta publictesting"
+    set "current_version=Public Testing"
+) else (
+    set "beta_params="
+    set "current_version=Public Release (Current Game Version)"
 )
-start "" /wait "%steamcmd_dir%\steamcmd.exe" +force_install_dir "%server_dir%" +login anonymous +app_update 996560 validate +quit
+echo %current_version% > "%version_file%"
+exit /b
+
+:download_server
+call :update_progress "Downloading Server..."
+start "" /wait "%steamcmd_dir%\steamcmd.exe" +force_install_dir "%server_dir%" +login anonymous %beta_params% +app_update 996560 validate +quit
 if not exist "%server_dir%\LocalAdmin.exe" (
-    echo.
-    echo ERROR: Failed to download server files!
+    echo ERROR: Server installation failed!
     timeout /t 5
     exit /b 1
 )
 set /a "current_step+=1"
 exit /b
 
+:management_menu
+cls
+echo ===== Server Management =====
+echo Current Version: %current_version%
+echo.
+echo 1. Check for updates
+echo 2. Change version
+echo 3. Open documentation
+echo 4. Exit
+choice /c 1234 /n /m "Select option [1-4]: "
+
+if errorlevel 4 exit /b
+if errorlevel 3 (
+    start "" "https://nwbooktest.itsgamertime.xyz/books/servers"
+    goto :management_menu
+)
+if errorlevel 2 (
+    call :select_version
+    call :update_server
+    goto :management_menu
+)
+if errorlevel 1 (
+    call :update_server
+    goto :management_menu
+)
+
+:update_server
+call :update_progress "Checking for updates..."
+set "temp_file=%temp%\version.tmp"
+dir "%server_dir%\LocalAdmin.exe" > "%temp_file%"
+
+start "" /wait "%steamcmd_dir%\steamcmd.exe" +force_install_dir "%server_dir%" +login anonymous %beta_params% +app_update 996560 validate +quit
+
+dir "%server_dir%\LocalAdmin.exe" > "%temp_file%.new"
+fc "%temp_file%" "%temp_file%.new" >nul
+if errorlevel 1 (
+    echo Update successful!
+    echo %current_version% > "%version_file%"
+) else (
+    echo Server is already up to date!
+)
+del "%temp_file%" "%temp_file%.new" 2>nul
+timeout /t 2 >nul
+exit /b
+
 :initialize_display
 cls
 echo ==================================================
-echo      Northwood SCP:SL Server Creator
+echo      Northwood SCP:SL Server Manager
 echo --------------------------------------------------
 echo                     Progress                     
 echo.                                                 
@@ -89,7 +169,7 @@ for %%a in ("%spinner_chars%") do (
 )
 cls
 echo ==================================================
-echo      Northwood SCP:SL Server Creator
+echo      Northwood SCP:SL Server Manager
 echo --------------------------------------------------
 echo                     Progress                     
 echo.                                                 
@@ -101,12 +181,18 @@ exit /b
 :complete_display
 cls
 echo ==================================================
-echo      Northwood SCP:SL Server Creator
+echo      Installation Complete!                      
 echo --------------------------------------------------
-echo                     Progress                     
-echo.                                                 
-echo  Current Task: Installation Complete!            
+echo  Installed Version: %current_version%
+echo  Install Location:  %server_dir%
 echo  [########################################] 100%% 
 echo ==================================================
-timeout /t 2 >nul
-goto :show_wiki_prompt
+timeout /t 3 >nul
+exit /b
+
+:post_install_prompt
+echo.
+choice /c yn /n /m "Would you like to open the official guide? (Y/N): "
+if errorlevel 2 exit /b
+start "" "https://nwbooktest.itsgamertime.xyz/books/servers"
+exit /b
